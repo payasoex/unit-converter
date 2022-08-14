c7f50d13dde51a068a309a4c1b29985e4a7744d3
Javier Retamal <javier.retamal.fontannaz@gmail.com>
2022-08-13 20:34:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
 const convertEl = document.getElementById("convert-el");
 const convertBtn = document.getElementById('convert-btn');
 const lengthEl = document.getElementById('length-el');
@@ -21,11 +15,9 @@ const literToGallon = 0.264;
 const kilogramToPound = 2.204;
 
 convertBtn.addEventListener('click', () => {
-    console.log('convert');
-    let baseValue = convertEl.value;
-
+    let baseValue = Number(convertEl.value);
+    
     lengthEl.textContent = `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feets | ${baseValue} feets = ${(baseValue / meterToFeet).toFixed(3)} meters`;
     volumeEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(3)} gallons | ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(3)} liters`;
     massEl.textContent = `${baseValue} kilogramss = ${(baseValue * kilogramToPound).toFixed(3)} pounds | ${baseValue} pounds = ${(baseValue / kilogramToPound).toFixed(3)} kilograms`;
-
 } );
